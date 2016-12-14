4f69a766b6c6ca63b348b43ced15afe7679ff76c
geetha0794 <geetha0794@yahoo.com>
2016-12-15 02:47:02 +0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -29,7 +29,7 @@ public class Login {
             return true;
         else
             return false;
-        //return return
+        //purpose of testing
     }
 
     public void deactivateAccount()
